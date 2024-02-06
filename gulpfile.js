10ac8085e6fd6b8d1939e6ff11a28d52d36613eb
neo <neo@gmail.com>
2024-02-06 16:04:02 +0300

    livereload added

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ import imagemin from 'gulp-imagemin';
 import fileinclude from "gulp-file-include";
 import webp from "gulp-webp";
 import htmlmin from "gulp-htmlmin";
+import livereload from "gulp-server-livereload";
 
 
 
@@ -26,8 +27,26 @@ const img = () => {
 const watcher = () => {
     gulp.watch('./src/html/**/*', html)
 };      
+const server = () => {
+    return gulp.src('./public/')
+    .pipe(livereload({
+        livereload: true,
+        open: true
+    }))
+};      
+// const dev = gulp.series (
+//     html,
+//     watcher
+// )
 
 
 export {html} ;
 export {img} ;
 export {watcher} ;
+export {server} ;
+export const dev = gulp.series(
+    html,
+    gulp.parallel(server, watcher)
+    );
+
+export default dev;
