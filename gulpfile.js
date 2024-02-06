d9ff9f687775ab65e8babd8624366cb1089bbe51
neo <neo@gmail.com>
2024-02-06 21:41:56 +0300

    added sassGlob

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,55 @@ import fileinclude from "gulp-file-include";
 import webp from "gulp-webp";
 import htmlmin from "gulp-htmlmin";
 import livereload from "gulp-server-livereload";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import {deleteAsync} from 'del';
+import sourcemaps from  "gulp-sourcemaps";
+import sassGlob from  "gulp-sass-glob";
+
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+const sass = gulpSass(dartSass);
 
 
 
 
 const html = () => {
     return gulp.src('./src/html/*.html')
+    .pipe(plumber({
+        errorHandler: notify.onError({
+            title: "Styles",
+            message:"Error: <%= error.message %>",
+            sound: false
+            
+        })}))
     .pipe(fileinclude())
     .pipe(htmlmin({ collapseWhitespace: true }))
      .pipe(gulp.dest('./public'))
 };
+
+
+const scss = () => {
+    return gulp.src('./src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass())
+    //.pipe(mediaqueries())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('.public/css/'))
+}
+
+
 const img = () => {
     return gulp.src('./src/img/**/*')
     .pipe(webp())
     .pipe(imagemin())
     .pipe(gulp.dest('./public/img'))
+};
+
+const clean = () => {
+    return deleteAsync('./public/')
+   
 };
 
 
@@ -42,9 +76,14 @@ const server = () => {
 
 export {html} ;
 export {img} ;
+export {clean} ;
 export {watcher} ;
 export {server} ;
+export {scss} ;
+
+
 export const dev = gulp.series(
+    clean,
     html,
     gulp.parallel(server, watcher)
     );
