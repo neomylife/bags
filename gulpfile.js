3bf73d4bf196d3be15031eae14d4500f10cbd4bc
neo <neo@gmail.com>
2024-02-06 13:15:24 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+import gulp from 'gulp';
+
+
+import imagemin from 'gulp-imagemin';
+import fileinclude from "gulp-file-include";
+import webp from "gulp-webp";
+
+
+
+
+const html = () => {
+    return gulp.src('./src/html/*.html')
+    .pipe(fileinclude())
+     .pipe(gulp.dest('./public'))
+};
+const img = () => {
+    return gulp.src('./src/img/**/*')
+    .pipe(webp())
+    .pipe(imagemin())
+    .pipe(gulp.dest('./public/img'))
+};
+
+
+export {html} ;
+export {img} ;
